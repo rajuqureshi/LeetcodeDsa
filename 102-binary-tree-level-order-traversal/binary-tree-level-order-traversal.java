bca3efa673a3c8a5f563c6c20139d37ff9e75a13
rajuqureshi <rajqureshi02@gmail.com>
2026-09-15 16:37:43 +0530

    Time: 33 ms (1.65%) | Memory: 46.7 MB (80.02%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int levels(TreeNode root){
+        if(root==null) return 0;
+        return 1+Math.max(levels(root.left),levels(root.right));
+    }
+    public void nThLevel(TreeNode root,int cur,int level,List<Integer> list){
+        if(root==null) return;
+        if(cur==level){
+            list.add(root.val);
+        }
+        nThLevel(root.left,cur+1,level,list);
+        nThLevel(root.right,cur+1,level,list);
+    }
+    public void levelsOrder(TreeNode root,List<List<Integer>> ans){
+        int levels = levels(root);
+        for(int i=1;i<=levels;i++){
+            ArrayList<Integer> list = new ArrayList<>();
+            nThLevel(root,1,i,list);
+            ans.add(list);
+        }
+    }
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        levelsOrder(root,ans);
+        return ans;
+    }
+}
